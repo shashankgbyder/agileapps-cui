141d77c301c25f1d265914583ac92a7483dd43ee
Satheesh Dominic (Satz) <satheeshdom@gmail.com>
2019-10-18 13:32:35 +0530

    Updated the simple template

--- src/simple/assets/js/scripts.js
+++ src/simple/assets/js/scripts.js
@@ -1,1 +1,22 @@
-console.log("Custom User Interface (CUI) development")
+console.log("Custom User Interface (CUI) development");
+
+
+// login event Handler
+
+const loginFormElm = document.querySelector('#LoginFormElm');
+const loginMsgContainer = document.querySelector('#loginMsgContainer');
+
+//success handler
+loginFormElm.addEventListener('loginSuccess', function (eventData) {
+    console.log(eventData)
+    loginMsgContainer.innerHTML = '<span class="text-success" role="alert"> Login successful! </span>';
+
+})
+
+//Error handler
+loginFormElm.addEventListener('loginError', function (eventData) {
+    console.log(eventData)
+    const errorMessage = eventData['detail']['error']['__exception_msg__'];
+    loginMsgContainer.innerHTML = '<span class="text-danger" role="alert">'+errorMessage+' </span>';
+
+})
